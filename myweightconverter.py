49116d5c6a17a59c38bbbbafc351e0aeae8b398e
ViniciusHBC <100978033+ViniciusHBC@users.noreply.github.com>
2022-05-05 16:26:00 -0300

    Added ounces & pounds
    Made it aesthetic friendlier & added up more measures, still not close of what i'm aiming. =P

--- myweightconverter.py
+++ myweightconverter.py
@@ -1,7 +1,22 @@
 weight = input('Type the weight in KG here: ')
 grams = isinstance(weight, int)
+ounces = isinstance(weight, int)
+pounds = isinstance(weight, int)
 if grams == True:
     grams = int(weight) * 1000
+    print((int(weight)), 'Kg in grams are ', grams, 'grams')
 else:
     grams = float(weight) * 1000
-print(grams)
+    print((float(weight)), 'Kg in grams are ', grams, 'grams')
+if ounces == True:
+    ounces = int(weight) * 35.27396195
+    print((int(weight)), 'Kg in ounces are ', ounces, 'ounces')
+else:
+    ounces = float(weight) * 35.27396195
+    print((float(weight)), 'Kg in ounces are ', round(ounces, 2), 'ounces')
+if pounds == True:
+    pounds = int(weight) * 2.20462262185
+    print((int(weight)), 'Kg in pounds are ', pounds, 'pounds')
+else:
+    pounds = float(weight) * 2.20462262185
+    print((float(weight)), 'Kg in pounds are ', round(pounds, 2), 'pounds')
