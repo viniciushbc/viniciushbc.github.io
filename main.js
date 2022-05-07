8d39054c4306651b17fdd9e2257fc4b4b11bc5a6
ViniciusHBC <100978033+ViniciusHBC@users.noreply.github.com>
2022-05-07 10:22:28 -0300

    changed to JS instead of Python
    A couple of changes in the html structure.
    
    Changed to Js because I didn't figure out how to use PyScript properly for this.

---
+++ main.js
@@ -0,0 +1,14 @@
+function selectweight() {
+    var value = document.getElementById("weightvalue").value;
+    console.log(value);
+}
+
+function selectmeasure1() {
+    var meas1 = document.getElementById("sel1").textContent;
+    console.log(meas1);
+}
+
+function selectmeasure2() {
+    var meas2 = document.getElementById("sel2").textContent;
+    console.log(meas2);
+}
