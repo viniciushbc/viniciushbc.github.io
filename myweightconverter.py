33a80cf4a9274a332db91fff0fb26da05b1af758
ViniciusHBC <100978033+ViniciusHBC@users.noreply.github.com>
2022-05-07 08:48:00 -0300

    Conversion from Kg to g removed
    useless

--- myweightconverter.py
+++ myweightconverter.py
@@ -1,13 +1,6 @@
 weight = input('Type the weight in KG here: ')
-grams = isinstance(weight, int)
 ounces = isinstance(weight, int)
 pounds = isinstance(weight, int)
-if grams == True:
-    grams = int(weight) * 1000
-    print((int(weight)), 'Kg in grams are ', grams, 'grams')
-else:
-    grams = float(weight) * 1000
-    print((float(weight)), 'Kg in grams are ', grams, 'grams')
 if ounces == True:
     ounces = int(weight) * 35.27396195
     print((int(weight)), 'Kg in ounces are ', ounces, 'ounces')
