b904a454e5b402dc41b283fb21c238c566b8410f
ViniciusHBC <100978033+ViniciusHBC@users.noreply.github.com>
2022-05-24 15:38:41 -0300

    Fixed Js file

--- main.js
+++ main.js
@@ -3,12 +3,19 @@ function selectweight() { //Prints the number written for organizingation purpos
     console.log(valor);
 }
 
-function submitfunction () {    //gets the text written and transforms it to Int//
-    var valor = document.getElementById("weightvalue").value;
-    var number = parseInt(valor);
-    console.log(number * 1000);
+function unitselected() {
+    var u = document.getElementById("unitFrom").value;
+    console.log(u);
 }
 
-let x = document.forms["Option2"];
-if (x = true)
-    console.log("o");
+function unitselected2() {
+    var r = document.getElementById("unitTo").value;
+    console.log(r);
+}
+
+function submitfunction () {    
+    var valor = document.getElementById("weightvalue").value;
+    var number = parseFloat(valor);
+    console.log(number * 1000);
+
+}
