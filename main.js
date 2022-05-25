67e6aa2e157294a6858da271db6a212a548a0445
ViniciusHBC <100978033+ViniciusHBC@users.noreply.github.com>
2022-05-25 09:29:54 -0300

    ITS WORKING PROPERLY!!
    Js code almost complete!! =P
    Just need to print the result out to the HTML and its done!

--- main.js
+++ main.js
@@ -1,21 +1,40 @@
-function selectweight() { //Prints the number written for organizingation purposes (this function should be deleted soon)//         
+function submitfunction () {
     var valor = document.getElementById("weightvalue").value;
-    console.log(valor);
-}
-
-function unitselected() {
     var u = document.getElementById("unitFrom").value;
-    console.log(u);
-}
-
-function unitselected2() {
-    var r = document.getElementById("unitTo").value;
-    console.log(r);
-}
-
-function submitfunction () {    
-    var valor = document.getElementById("weightvalue").value;
+    var r = document.getElementById("unitTo").value; 
     var number = parseFloat(valor);
-    console.log(number * 1000);
-
+    
+    if (u == r) {
+        console.log ("Same measure!!");
+    }
+    if (u == "Option 1") {
+        if (r == "Option 2") {
+            const kgtooz = (number * 35.27396195);
+            console.log(kgtooz);
+        }
+        if (r == "Option 3") {
+            const kgtolb = (number * 2.20462262185);
+            console.log(kgtolb);
+        }
+    }
+    if (u == "Option 2") {
+        if (r == "Option 1") {
+            const oztokg = (number * 0.02834952);
+            console.log(oztokg);
+        }
+        if (r == "Option 3") {
+            const oztolb = (number * 0.0625);
+            console.log(oztolb);
+        }
+    }
+    if (u == "Option 3") {
+        if (r == "Option 1") {
+            const lbtokg = (number * 0.453592);
+            console.log(lbtokg);
+         }
+        if (r == "Option 2") {
+            const lbtooz = (number * 16);
+            console.log(lbtooz);
+        }
+    }
 }
